98d2be98dcb48571988bd5a29582b40599d241e8
mcskinner <git@mcskinner.com>
2016-10-15 12:54:21 -0700

    pluck the ETS model building out into a reusable method

--- src/model.py
+++ src/model.py
@@ -1,6 +1,38 @@
 import numpy as np
 import tensorflow as tf
 
+# Build out the TensorFlow model for Holt's linear method.
+def ModelETS(w_in, F_in):
+    n = len(w_in)
+
+    def model(data):
+        num_steps = data.get_shape()[0]
+
+        # ETS params
+        state0 = tf.Variable(tf.random_uniform([n, 1], -0.1, 0.1), dtype = tf.float32)
+        params = tf.Variable(tf.random_uniform([n, 1], 0.1, 0.2), dtype = tf.float32)
+        w = tf.constant(w_in, shape = [n, 1], dtype = tf.float32)
+        F = tf.constant(F_in, shape = [n, n], dtype = tf.float32)
+
+        # Unrolled ETS loop
+        outputs = []
+        state = state0
+        for time_step in range(num_steps):
+            output = tf.matmul(tf.transpose(w), state)
+            state = tf.matmul(F, state) + params * (data[time_step] - output)
+            outputs.append(output)
+
+        # Sum of squared errors (SSE)
+        pack_out = tf.reshape(tf.pack(outputs), data.get_shape())
+        cost = tf.reduce_sum(tf.pow(pack_out - data, 2))
+        return state0, params, cost
+
+    return model
+
+
+SimpleETS = ModelETS([1], [[1]])
+HoltsLinear = ModelETS([1, 1], [[1, 1], [0, 1]])
+
 # Meta-parameters and debugging knobs
 learning_rate = 0.01
 training_epochs = 2000
@@ -8,30 +40,10 @@ display_step = 50
 
 # Test data
 ys = np.asarray([1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-num_steps = ys.shape[0]
-
-# Input data placeholder
-data = tf.placeholder('float')
-
-# ETS params
-state0 = tf.Variable(tf.random_uniform([2, 1], -0.1, 0.1), dtype = tf.float32)
-params = tf.Variable(tf.random_uniform([2, 1], 0.1, 0.2), dtype = tf.float32)
-w = tf.constant([1, 1], shape = [2, 1], dtype = tf.float32)
-F = tf.constant([[1, 1], [0, 1]], dtype = tf.float32)
-
-# Unrolled ETS loop
-outputs = []
-state = state0
-for time_step in range(num_steps):
-    output = tf.matmul(tf.transpose(w), state)
-    state = tf.matmul(F, state) + params * (data[time_step] - output)
-    outputs.append(output)
-
-# Mean squared error
-pack_out = tf.reshape(tf.pack(outputs), [num_steps])
-cost = tf.reduce_sum(tf.pow(pack_out - data, 2))
+data = tf.placeholder('float', ys.shape)
 
 # Gradient descent
+state0, params, cost = HoltsLinear(data)
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
 # Initializing the variables
