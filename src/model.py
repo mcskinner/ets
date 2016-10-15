adcef0aec9f08020460749f726ea9738cc50d3ea
mcskinner <git@mcskinner.com>
2016-10-15 15:11:18 -0700

    add support for a damped trend model (primitive vars support)

--- src/model.py
+++ src/model.py
@@ -1,18 +1,36 @@
+import itertools
 import numpy as np
 import tensorflow as tf
+rng = np.random
+
+# recursively flatten a list
+def flatten(l):
+    if l == []:
+        return l
+    if isinstance(l[0], list):
+        return flatten(l[0]) + flatten(l[1:])
+    return l[:1] + flatten(l[1:])
+
 
 # Build out the TensorFlow model for Holt's linear method.
 def ModelETS(w_in, F_in):
+    w_in = flatten(w_in)
+    F_in = flatten(F_in)
+    var_names = [v for v in itertools.chain(w_in, F_in) if isinstance(v, str)]
     n = len(w_in)
 
     def model(data):
         num_steps = data.get_shape()[0]
+        varz = dict((var, tf.Variable(rng.uniform(0, 1), name = var, dtype = tf.float32)) for var in var_names)
+        get_var = lambda v: (isinstance(v, str) and varz[v]) or tf.constant(v, dtype = tf.float32)
+        w_varz = tf.pack([get_var(v) for v in w_in])
+        F_varz = tf.pack([get_var(v) for v in F_in])
 
         # ETS params
         state0 = tf.Variable(tf.random_uniform([n, 1], -0.1, 0.1), dtype = tf.float32)
         params = tf.Variable(tf.random_uniform([n, 1], 0.1, 0.2), dtype = tf.float32)
-        w = tf.constant(w_in, shape = [n, 1], dtype = tf.float32)
-        F = tf.constant(F_in, shape = [n, n], dtype = tf.float32)
+        w = tf.reshape(w_varz, shape = [n, 1])
+        F = tf.reshape(F_varz, shape = [n, n])
 
         # Unrolled ETS loop
         outputs = []
@@ -25,16 +43,18 @@ def ModelETS(w_in, F_in):
         # Sum of squared errors (SSE)
         pack_out = tf.reshape(tf.pack(outputs), data.get_shape())
         cost = tf.reduce_sum(tf.pow(pack_out - data, 2))
-        return state0, params, cost
+        return state0, params, varz, cost
 
     return model
 
 
 SimpleETS = ModelETS([1], [[1]])
 HoltsLinear = ModelETS([1, 1], [[1, 1], [0, 1]])
+DampedTrend = ModelETS([1, 'phi'], [[1, 'phi'], [0, 'phi']])
+
 
 # Meta-parameters and debugging knobs
-learning_rate = 0.01
+learning_rate = 0.001
 training_epochs = 2000
 display_step = 50
 
@@ -43,8 +63,18 @@ ys = np.asarray([1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 data = tf.placeholder('float', ys.shape)
 
 # Gradient descent
-state0, params, cost = HoltsLinear(data)
+state0, params, varz, cost = DampedTrend(data)
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+
+def PrintDiagnostics(sess, ys, epoch_str):
+    c = sess.run(cost, feed_dict={data: ys})
+    print "Epoch:", epoch_str, \
+        "cost=", "{:.9f}".format(c), \
+        "state0=", sess.run(tf.reshape(state0, [2])), \
+        "params=", sess.run(tf.reshape(params, [2])), \
+        "varz=", dict((k, sess.run(v)) for k, v in varz.iteritems())
+
 
 # Initializing the variables
 init = tf.initialize_all_variables()
@@ -59,14 +89,7 @@ with tf.Session() as sess:
 
         # Display logs per epoch step
         if (epoch + 1) % display_step == 0:
-            c = sess.run(cost, feed_dict={data: ys})
-            print "Epoch:", '%04d' % (epoch+1), \
-                "cost=", "{:.9f}".format(c), \
-                "state0=", sess.run(tf.reshape(state0, [2])), \
-                "params=", sess.run(tf.reshape(params, [2]))
+            PrintDiagnostics(sess, ys, '%04d' % (epoch+1))
 
     print "Optimization Finished!"
-    training_cost = sess.run(cost, feed_dict={data: ys})
-    print "Training cost=", training_cost, \
-        "state0=", sess.run(tf.reshape(state0, [2])), \
-        "params=", sess.run(tf.reshape(params, [2]))
+    PrintDiagnostics(sess, ys, 'Final')
