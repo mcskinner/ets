e76e67cdcfc2c4e14afc3d70b1c2303027746a65
mcskinner <git@mcskinner.com>
2016-10-15 12:29:54 -0700

    re-parameterize Holt's linear method using matrices

--- src/model.py
+++ src/model.py
@@ -1,6 +1,5 @@
 import numpy as np
 import tensorflow as tf
-rng = np.random
 
 # Meta-parameters and debugging knobs
 learning_rate = 0.01
@@ -8,35 +7,29 @@ training_epochs = 2000
 display_step = 50
 
 # Test data
-y = np.asarray([1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-num_steps = y.shape[0]
+ys = np.asarray([1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+num_steps = ys.shape[0]
 
-# Input data placeholders
-data_in = tf.placeholder('float')
-data_out = tf.placeholder('float')
+# Input data placeholder
+data = tf.placeholder('float')
 
 # ETS params
-level0 = tf.Variable(0.1 * rng.randn(), name = 'level0', dtype = tf.float32)
-pace0 = tf.Variable(0.1 * rng.randn(), name = 'pace0', dtype = tf.float32)
-alpha = tf.Variable(0.5, name = 'alpha', dtype = tf.float32)
-beta = tf.Variable(0.1, name = 'beta', dtype = tf.float32)
-
-# Definition of the ETS update
-def update(y, level, pace):
-    output = level + pace
-    new_level = output + alpha * (y - output)
-    new_pace = pace + beta * (y - output)
-    return output, new_level, new_pace
+state0 = tf.Variable(tf.random_uniform([2, 1], -0.1, 0.1), dtype = tf.float32)
+params = tf.Variable(tf.random_uniform([2, 1], 0.1, 0.2), dtype = tf.float32)
+w = tf.constant([1, 1], shape = [2, 1], dtype = tf.float32)
+F = tf.constant([[1, 1], [0, 1]], dtype = tf.float32)
 
 # Unrolled ETS loop
 outputs = []
-level, pace = level0, pace0
+state = state0
 for time_step in range(num_steps):
-    output, level, pace = update(data_in[time_step], level, pace)
+    output = tf.matmul(tf.transpose(w), state)
+    state = tf.matmul(F, state) + params * (data[time_step] - output)
     outputs.append(output)
 
 # Mean squared error
-cost = tf.reduce_sum(tf.pow(tf.pack(outputs) - data_out, 2))
+pack_out = tf.reshape(tf.pack(outputs), [num_steps])
+cost = tf.reduce_sum(tf.pow(pack_out - data, 2))
 
 # Gradient descent
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
@@ -50,22 +43,18 @@ with tf.Session() as sess:
 
     # Fit the data.    
     for epoch in range(training_epochs):
-        sess.run(optimizer, feed_dict={data_in: y, data_out: y})
+        sess.run(optimizer, feed_dict={data: ys})
 
         # Display logs per epoch step
         if (epoch + 1) % display_step == 0:
-            c = sess.run(cost, feed_dict={data_in: y, data_out: y})
+            c = sess.run(cost, feed_dict={data: ys})
             print "Epoch:", '%04d' % (epoch+1), \
                 "cost=", "{:.9f}".format(c), \
-                "level0=", sess.run(level0), \
-                "pace0=", sess.run(pace0), \
-                "alpha=", sess.run(alpha), \
-                "beta=", sess.run(beta)
+                "state0=", sess.run(tf.reshape(state0, [2])), \
+                "params=", sess.run(tf.reshape(params, [2]))
 
     print "Optimization Finished!"
-    training_cost = sess.run(cost, feed_dict={data_in: y, data_out: y})
+    training_cost = sess.run(cost, feed_dict={data: ys})
     print "Training cost=", training_cost, \
-        "level0=", sess.run(level0), \
-        "pace0=", sess.run(pace0), \
-        "alpha=", sess.run(alpha), \
-        "beta=", sess.run(beta)
+        "state0=", sess.run(tf.reshape(state0, [2])), \
+        "params=", sess.run(tf.reshape(params, [2]))
