6db6b4ec04fb33bb52e02f4b3e477e3c9b9a5f07
mcskinner <git@mcskinner.com>
2016-11-05 18:24:32 -0700

    simplify GradientDescent model, extract to function

--- src/optimize.py
+++ src/optimize.py
@@ -3,26 +3,18 @@ import ets
 import models
 
 import numpy as np
+import scipy.optimize
 import tensorflow as tf
 
 
 # Meta-meta-parameters and debugging knobs.
 training_epochs = 10000
-half_life = 750
-display_step = 100
-gradient_clip = 2.0
-hot_ticket = 1
+half_life = 1500
 cost_growth = 1.5
-base_learn = 0.0025
-peak_param_cost = 1
+base_learn = 0.005
+base_cost_weight = 5.0
 
-
-# Meta-parameters for the optimization, themselves governed by the meta-meta-parameters above.
-global_step = tf.Variable(0)
-learning_rate = tf.minimum(
-    tf.train.exponential_decay(base_learn * 2, global_step, half_life * hot_ticket, 0.5),
-    tf.train.exponential_decay(base_learn * (2**hot_ticket), global_step, half_life, 0.5))
-cost_weight = tf.train.exponential_decay(peak_param_cost / (cost_growth**hot_ticket), global_step, half_life, cost_growth)
+display_step = 100
 
 
 # Set up the input data.
@@ -33,11 +25,9 @@ data = tf.placeholder('float', ys.shape)
 
 
 # And then build the model.
+global_step = tf.Variable(0, name = 'global_step')
+cost_weight = tf.train.exponential_decay(base_cost_weight, global_step, half_life, cost_growth)
 state0, varz, cost = models.BaselineState(cost_weight)(data)
-
-
-# Gradient descent, with decaying learning rate.
-optimizer = tf.train.AdamOptimizer(learning_rate, beta2=0.9).minimize(cost, global_step=global_step)
 
 
 # Dump the state to the screen.
@@ -54,19 +44,27 @@ def PrintDiagnostics(sess, ys, title):
 init = tf.initialize_all_variables()
 
 
-# Launch the graph
-with tf.Session() as sess:
-    sess.run(init)
-    PrintDiagnostics(sess, ys, 'Init')
+# Gradient descent, with decaying learning rate.
+def GradientDescent():
+    # Meta-parameters for the optimization, themselves governed by the meta-meta-parameters above.
+    learning_rate = tf.train.exponential_decay(base_learn, global_step, half_life, 0.5)
+    optimizer = tf.train.AdamOptimizer(learning_rate, beta2=0.9).minimize(cost, global_step=global_step)
 
-    # Fit the data.    
-    for epoch in range(training_epochs):
-        sess.run(optimizer, feed_dict={data: ys})
+    # Launch the graph
+    with tf.Session() as sess:
+        sess.run(init)
+        PrintDiagnostics(sess, ys, 'Init')
 
-        # Display logs per epoch step
-        if epoch % display_step == 0:
-            PrintDiagnostics(sess, ys, 'Epoch: %04d' % epoch)
+        # Fit the data.    
+        for epoch in range(training_epochs):
+            sess.run(optimizer, feed_dict={data: ys})
 
-    print "Optimization Finished!"
-    sess.run(optimizer, feed_dict={data: ys})
-    PrintDiagnostics(sess, ys, 'Training')
+            # Display logs per epoch step
+            if epoch % display_step == 0:
+                PrintDiagnostics(sess, ys, 'Epoch: %04d' % epoch)
+
+        print "Optimization Finished!"
+        PrintDiagnostics(sess, ys, 'Training')
+
+
+GradientDescent()
